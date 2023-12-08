19af2b4b5ecabcdc856d47a3c4ad85eaad369a01
kurekagura <26186258+kurekagura@users.noreply.github.com>
2023-12-09 03:32:30 +0900

    selectの工夫。

--- I18N/README.md
+++ I18N/README.md
@@ -6,6 +6,24 @@ RequestLocalizationOptionsの既定では、RequestCultureProvidersに以下の�
 - CookieRequestCultureProvider
 - AcceptLanguageHeaderRequestCultureProvider
 
+## select要素
+
+組込み型SelectListとTupleを使うのがシンプル。
+
+バインドが不要な場合は選択した値のためのModelのプロパティ宣言をなくせる（Ajax利用目的）。但し、label for="id"のためにidを直に指定する必要がある。
+
+```html
+<form>
+    <label asp-for="@Model.name_SelectedSelectSeasonItem"></label>
+    <select asp-for="@Model.name_SelectedSelectSeasonItem" asp-items="@Model.SelectSeasonItems"></select>
+</form>
+
+<form>
+    <label asp-for="@Model.SelectSeasonItems2"></label>
+    <select id="@nameof(Model.SelectSeasonItems2)" asp-items="@Model.SelectSeasonItems2"></select>
+</form>
+```
+
 ## 深謝
 
 - [Enumに定義してあるDisplay属性を表示する。リソースファイルがある場合、リソースから取得する拡張メソッド](https://qiita.com/mak_in/items/7909e51d249826115403)
--- I18N/WebApp/Pages/Enum/Index.cshtml.cs
+++ I18N/WebApp/Pages/Enum/Index.cshtml.cs
@@ -10,10 +10,16 @@ namespace WebApp.Pages.Enum;
 [Display(Name = "PAGE_TITLE_ENUM")]
 public class IndexModel : PageModel
 {
-    public SelectList SelectSeasonItems { get; set; }
+    #region 属性付与の位置に注意
+    [Display(Name = "季節の選択（Display属性）")]
+    public SelectList SelectSeasonItems2 { get; set; }
+    #endregion
 
+    #region 属性付与の位置に注意
+    public SelectList SelectSeasonItems { get; set; }
     [Display(Name = "季節の選択（Display属性）")]
     public EnumSeason name_SelectedSelectSeasonItem { set; get; }
+    #endregion
 
     public List<EnumSeason> name_RadioSeasonItems { set; get; } = new();
 
@@ -22,19 +28,32 @@ public class IndexModel : PageModel
 
     public IndexModel()
     {
+        SelectSeasonItems2 = new SelectList(new List<SeasonValueText>
+        {
+            new SeasonValueText{Value=EnumSeason.Spring, Text=EnumSeason.Spring.DisplayName() },
+            new SeasonValueText{Value=EnumSeason.Summer, Text=EnumSeason.Summer.DisplayName() },
+            new SeasonValueText{Value=EnumSeason.Autumn, Text=EnumSeason.Autumn.DisplayName() },
+            new SeasonValueText{Value= EnumSeason.Winter, Text=EnumSeason.Winter.DisplayName() }
+        },
+        dataValueField: nameof(SeasonValueText.Value),
+        dataTextField: nameof(SeasonValueText.Text),
+        selectedValue: nameof(EnumSeason.Summer)    //バインドしない場合初期選択はここ
+        );
+
         //select要素では.NETのSelectListを利用できる。Tupleが使える。ValueTapleはフィールドなのでNG?
         //Razorの@Html.DisplayTextForでリソース対応できなくはなさそう（asp-itemsは使えなくなる？）
-        SelectSeasonItems = new SelectList(new List<Tuple<string, string>>()
+        SelectSeasonItems = new SelectList(new List<Tuple<EnumSeason, string>>()
         {
-            new (nameof(EnumSeason.Spring), EnumSeason.Spring.DisplayName() ),
-            new (nameof(EnumSeason.Summer), EnumSeason.Summer.DisplayName() ),
-            new (nameof(EnumSeason.Autumn), EnumSeason.Autumn.DisplayName() ),
-            new (nameof(EnumSeason.Winter), EnumSeason.Winter.DisplayName() )
+            new (EnumSeason.Spring, EnumSeason.Spring.DisplayName() ),
+            new (EnumSeason.Summer, EnumSeason.Summer.DisplayName() ),
+            new (EnumSeason.Autumn, EnumSeason.Autumn.DisplayName() ),
+            new (EnumSeason.Winter, EnumSeason.Winter.DisplayName() )
         },
         dataValueField: "Item1",
         dataTextField: "Item2",
-        selectedValue: nameof(EnumSeason.Summer) //ここ機能してない（Tupleはダメ？）
+        selectedValue: nameof(EnumSeason.Summer)
         );
+        name_SelectedSelectSeasonItem = EnumSeason.Summer; //バインドする場合の初期選択はここ
 
         name_RadioSeasonItems.AddRange(new[] {
             EnumSeason.Spring, EnumSeason.Summer, EnumSeason.Autumn, EnumSeason.Winter});
@@ -43,4 +62,10 @@ public class IndexModel : PageModel
     public void OnGet()
     {
     }
+}
+
+public class SeasonValueText
+{
+    public EnumSeason Value { get; set; }
+    public string Text { get; set; } = default!;
 }
