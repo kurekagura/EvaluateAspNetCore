f8275d003590266e773da1f507965b948f035832
kurekagura <26186258+kurekagura@users.noreply.github.com>
2023-12-13 10:13:37 +0900

    部分ビュー検証用。

---
+++ PartialView/WebApp/Pages/Partial1/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages.Partial1
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
