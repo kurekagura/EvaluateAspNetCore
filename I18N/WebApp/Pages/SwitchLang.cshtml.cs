e81e9a86bb4759c020f188b6f053474d6ab0f8ad
kurekagura <26186258+kurekagura@users.noreply.github.com>
2024-01-03 15:53:34 +0900

    クッキーのクリアを実装。

--- I18N/WebApp/Pages/SwitchLang.cshtml.cs
+++ I18N/WebApp/Pages/SwitchLang.cshtml.cs
@@ -17,6 +17,19 @@ namespace WebApp.Pages
                     CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture))
                     );
             }
+            else
+            {
+                // 現在のクッキーを削除するために同じ名前のクッキーを新しく設定し、有効期限を過去に設定
+                Response.Cookies.Append(
+                    "lang",
+                    "", // 空文字列または null で新しい値を設定
+                    new CookieOptions
+                    {
+                        Expires = DateTime.Now.AddDays(-1), // 有効期限を過去の日時に設定
+                    }
+                );
+
+            }
 
             string returnUrl = Request.Headers["Referer"].ToString() ?? "/";
             Response.Redirect(returnUrl);
